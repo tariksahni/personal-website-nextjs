209604b0cd47b890323f7baae62eb266bf4d0889
Tarik <tariksahni@gmail.com>
2021-11-08 19:11:43 +0530

    feat: transfer home page to next

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="preload" as="image" href="/images/Tarik.jpg" />
+					<link rel="stylesheet" as="font" href="https://fonts.googleapis.com/css?family=Poppins:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" />
+					<link rel="stylesheet" as="font" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,600i,700" />
+					<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+					<link rel="manifest" href="/favicon/site.webmanifest" />
+					<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#ffffff" />
+				</Head>
+				<body>
+				<Main />
+				<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
+
+export default MyDocument
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const GITHUB_URL = 'https://github.com/tariksahni';
+export const LINKED_IN_URL = 'https://www.linkedin.com/in/tariksahni/';
+export const TWITTER_URL = 'https://twitter.com/tarik_sahni';
+export const FACEBOOK_URL = 'https://www.facebook.com/tarik.sahni.9';
+export const WHATSAPP_URL = 'https://wa.me/917831838003';
