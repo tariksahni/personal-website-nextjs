805b9a009a493f6e2ab99ecd25061c952ed472a7
Tarik <tariksahni@gmail.com>
2021-11-10 16:30:31 +0530

    feat: implemented pricing model

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,4 @@ export const LINKED_IN_URL = 'https://www.linkedin.com/in/tariksahni/';
 export const TWITTER_URL = 'https://twitter.com/tarik_sahni';
 export const FACEBOOK_URL = 'https://www.facebook.com/tarik.sahni.9';
 export const WHATSAPP_URL = 'https://wa.me/917831838003';
-export const MESSAGE_WALL_CONTRACT_ADDRESS = '0x2dc31F33ee8872fd121Cc85dC73c1E7bef731BcE';
+export const MESSAGE_WALL_CONTRACT_ADDRESS = '0xf1B015e975beD7c0adF21b5BEBf40E499bc16Ff1';
