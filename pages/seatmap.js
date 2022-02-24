eafc8a9e89c7ea231d13ec1a1668bd10c152a26a
Tarik <tariksahni@gmail.com>
2022-02-24 16:25:37 +0530

    feat: poc for seatmap- 2

--- pages/seatmap.js
+++ pages/seatmap.js
@@ -1,11 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, {useRef} from 'react';
 
 const SeatMap = () => {
+	const iframeNode = useRef(null);
+	const iframeOnLoad = () => {
+		const iframeWindow = iframeNode.current.contentWindow;
+		console.log("aaya check", iframeWindow, iframeNode);
+	}
 	return (
 		<div className={'seatmap-container'}>
 			<iframe
 				title="Seat Selection"
-				src={'https://www.headout.com/seatmap/tour-group/3023?date=2022-02-26&time=19:30:00&showOnly=true&currencyCode=USD'}
+				src={'https://www.test-headout.com/seatmap/tour-group/3023?date=2022-02-26&time=19:30:00&showOnly=true&currencyCode=USD'}
+				onLoad={iframeOnLoad}
+				ref={iframeNode}
 			></iframe>
 		</div>
 	)
